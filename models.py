2dcbbfeefc1942755aca54b95c9a9f27b3f1a50e
Gourab <ggourab2004@gmail.com>
2026-03-14 23:56:07 +0530

    Delete models.py

--- models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class PDFFILE(models.Model):
-    title = models.CharField(max_length=200)
-    pdf_file = models.FileField(upload_to = "pdfs/")
-    uploaded_at = models.DateTimeField(auto_now_add = True)
