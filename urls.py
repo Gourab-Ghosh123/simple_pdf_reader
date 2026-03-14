c6a2ff3c219b1948e5a2c878368b7f9d958f4123
Gourab <ggourab2004@gmail.com>
2026-03-14 23:56:48 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("/" , views.upload_pdf , name = "upload_pdf"),
-    path("read/<int : pdf_id>/<int : page_number>/" , views.read_pdf , name = "read_pdf"),
-]
